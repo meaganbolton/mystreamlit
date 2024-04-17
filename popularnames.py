3894ffc244393c7325e4fa04dadba05f28579a40
Meagan Bolton <157442147+meaganbolton@users.noreply.github.com>
2024-04-17 16:37:37 -0600

    Update popularnames.py
    Lets see

--- popularnames.py
+++ popularnames.py
@@ -9,6 +9,7 @@ st.title('Popular Name Trends')
 url = 'https://github.com/meaganbolton/-Unveiling-Insights-Exploring-Data-Science-Job-Market-Dynamics/blob/main/df_glassdoor.csv'
 df = pd.read_csv(url)
 
+
 city = st.text_input('Enter a city', value='Houston')
 ################################################################333
 # Filter data for the selected city
@@ -16,32 +17,32 @@ city_df = df[df['City'] == city]
 
 st.header(f'{city} over time')
 
-tab1, tab2 = st.tabs(['City','State'])
-# Calculate average, min, and max salaries for each city
-city_salary_stats = city_df.groupby('City').agg({'Salary Estimate': ['mean', 'min', 'max']}).reset_index()
-with tab1:
-    # plot_df = city_df[city_df['sex']=='F']
-    fig = px.bar(city_salary_stats, 
-                y='City', 
-                x=['mean', 'min', 'max'], 
-                orientation='h',
-                barmode='group',
-                labels={'value': 'Salary ($)', 'variable': 'Statistic'},
-                title=f'Salary Comparison for {city}',
-                color_discrete_map={'mean': 'blue', 'min': 'red', 'max': 'green'},
-                )
-    st.plotly_chart(fig)
-# Calculate average, min, and max salaries for each city
-state_salary_stats = city_df.groupby('State').agg({'Salary Estimate': ['mean', 'min', 'max']}).reset_index()
-with tab1:
-    # plot_df = city_df[city_df['sex']=='F']
-    fig = px.bar(state_salary_stats, 
-                y='City', 
-                x=['mean', 'min', 'max'], 
-                orientation='h',
-                barmode='group',
-                labels={'value': 'Salary ($)', 'variable': 'Statistic'},
-                title=f'Salary Comparison for {city}',
-                color_discrete_map={'mean': 'blue', 'min': 'red', 'max': 'green'},
-                )
-    st.plotly_chart(fig)
+# tab1, tab2 = st.tabs(['City','State'])
+# # Calculate average, min, and max salaries for each city
+# city_salary_stats = city_df.groupby('City').agg({'Salary Estimate': ['mean', 'min', 'max']}).reset_index()
+# with tab1:
+#     # plot_df = city_df[city_df['sex']=='F']
+#     fig = px.bar(city_salary_stats, 
+#                 y='City', 
+#                 x=['mean', 'min', 'max'], 
+#                 orientation='h',
+#                 barmode='group',
+#                 labels={'value': 'Salary ($)', 'variable': 'Statistic'},
+#                 title=f'Salary Comparison for {city}',
+#                 color_discrete_map={'mean': 'blue', 'min': 'red', 'max': 'green'},
+#                 )
+#     st.plotly_chart(fig)
+# # Calculate average, min, and max salaries for each city
+# state_salary_stats = city_df.groupby('State').agg({'Salary Estimate': ['mean', 'min', 'max']}).reset_index()
+# with tab1:
+#     # plot_df = city_df[city_df['sex']=='F']
+#     fig = px.bar(state_salary_stats, 
+#                 y='City', 
+#                 x=['mean', 'min', 'max'], 
+#                 orientation='h',
+#                 barmode='group',
+#                 labels={'value': 'Salary ($)', 'variable': 'Statistic'},
+#                 title=f'Salary Comparison for {city}',
+#                 color_discrete_map={'mean': 'blue', 'min': 'red', 'max': 'green'},
+#                 )
+#     st.plotly_chart(fig)
